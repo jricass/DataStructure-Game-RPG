92c4958502ed5793074ce92dc35c5b0a12696350
carlos eduardo cardoso silva <carlossilvaaeduuu@gmail.com>
2025-04-13 17:10:20 -0300

    Update Personagem.java

--- src/main/java/br/projeto/RPG/Personagem.java
+++ src/main/java/br/projeto/RPG/Personagem.java
@@ -1,61 +1,72 @@
-import java.util.Random;
+  import java.util.Scanner;
 
-public class Personagem {
-    int idPersonagem=0;
-    String nome;
-    int nivel=0;
-    int vidaMaxima,vidaAtual;
-    int manaMaxima,manaAtual;
+  public class Jogador {
+      int id=0;
+      double saldo_moedas;
+      String nome;
+      int senha;
 
-    public int receberDano(int dano){
-        vidaAtual-=dano;
+      Scanner scan = new Scanner(System.in);
+      LinkedList lista = new LinkedList();
+      Personagem personagem = new Personagem();
+      
 
-        if(vidaAtual<0){
-            vidaAtual=0;
-        }
-        return vidaAtual;
-    }
+      public void cadastrar(){
+          System.out.println("digite sua senha agr");
+          senha=scan.nextInt();
+          System.out.println("digite seu nome agr");
+          senha=scan.nextInt();
+          id++;
+      }
+
+      public boolean autenticar(){
+          System.out.println("digite seu id");
+          int temp=scan.nextInt();
+          System.out.println("digite sua senha agr");
+            int temp2=scan.nextInt();
+            
+            if(temp==id && temp2==senha){
+              System.out.println("autenticado com sucesso");
+              return true;
+            }else{
+              System.out.println("erro semnha ou id errado");
+              return false;
+            }
+
+      }
+
+      public void criarPersonagem() {
+        personagem = new Personagem();  
     
-    public int curar(int cura){
-        vidaAtual+=cura;
-
-        if(vidaAtual>vidaMaxima){
-            vidaAtual=vidaMaxima;
-        }
-        return vidaAtual;
+        System.out.println("Digite o nome do personagem:");
+        personagem.nome = scan.next();
+    
+        System.out.println("Digite o nível do personagem:");
+        personagem.nivel = scan.nextInt();
+    
+        personagem.setManaMaxima(); 
+        personagem.setVidaMaxima(); 
+    
+        lista.add(personagem);  
+        System.out.println("Personagem criado e adicionado à lista");
+        System.out.println(personagem.toString());
     }
 
+    public Personagem selecionar_personagem(){
+      int i=0;
 
-        public int setManaMaxima(){
-            Random random = new Random();
-            manaMaxima = random.nextInt(11) + nivel*10; 
-            manaAtual = manaMaxima; 
-            return manaMaxima;
-        }
-       
-    
-    
-    public int setVidaMaxima(){
-        Random random = new Random();
-        vidaMaxima = random.nextInt(11) + nivel*10; 
-        vidaAtual = vidaMaxima; 
-        return vidaMaxima;
-    }
+      System.out.println("digite o nome do personagem");
+      String temp = scan.next();
 
-    public void usarHabilidade(){
+      while(i<lista.size()){
+        i++;
+      }
+      if(temp.equals(lista.selecionar_index(i))){
+        System.out.println("personagem selecionado");
         
+        return lista.index(i);
+    }else{
+      return null;
     }
-
-   
-
-    @Override
-    public String toString() {
-        return "Personagem{" +
-                "id=" + idPersonagem +
-                ", nome='" + nome + '\'' +
-                ", nivel=" + nivel +
-                ", vida=" + vidaAtual + "/" + vidaMaxima +
-                ", mana=" + manaAtual + "/" + manaMaxima +
-                '}';
     }
-}
+  }
