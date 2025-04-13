c28377f1a499dea6d193bca8c84b7ec90c55a267
carlos eduardo cardoso silva <carlossilvaaeduuu@gmail.com>
2025-04-13 16:34:02 -0300

    Update LinkedList.java

--- src/main/java/br/projeto/RPG/LinkedList.java
+++ src/main/java/br/projeto/RPG/LinkedList.java
@@ -1,5 +1,6 @@
 public class LinkedList {
     Node head,tail;
+    Personagem personagem = new Personagem();
 
     public void add(Personagem personagem) {
         Node<Personagem> newNode = new Node<>(personagem);  // Cria um novo nó
@@ -14,4 +15,34 @@ public class LinkedList {
             tail = newNode;    
         }
     }
+
+    public int size() {
+        int contador = 0;
+        Node curr=head;
+
+        while (curr != null) {
+            curr = curr.next;
+            contador++;
+        }
+
+
+        return contador;
+    }
+
+    public String selecionar_index(int i) {
+        int index = 0;
+        Node temp = head;
+
+        while (temp != null && index < i) {
+            temp = temp.next;
+            index++;
+        }
+
+        if (temp != null) {
+            return temp.data.nome;
+        } else {
+            return null; // índice inválido
+        }
+    }
+
 }
